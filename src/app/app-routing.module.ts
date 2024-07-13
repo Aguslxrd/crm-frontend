93337312d85b01e04ec6c6c77dd6fd0b138494fc
damian <suffodamian02@gmail.com>
2024-07-13 20:15:07 -0300

    added: login form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './core/pages/home/home.component';
+import { LoginComponent } from './core/pages/login/login.component';
 
 const routes: Routes = [
   {path: "", component: HomeComponent},
-  {path: "home", component: HomeComponent}
+  {path: "home", component: HomeComponent},
+  {path: "login", component: LoginComponent}
 ];
 
 @NgModule({
