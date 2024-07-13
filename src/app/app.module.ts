7134b29b3b79a67b4a2e86f42a93f1330abdd530
damian <suffodamian02@gmail.com>
2024-07-13 00:22:02 -0300

    added: folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './core/components/navbar/navbar.component';
+import { HomeComponent } from './core/pages/home/home.component';
+import { LoginComponent } from './core/pages/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
