0e3d4e7fb0ff0a84c486194388c17461f4239c98
damian <suffodamian02@gmail.com>
2024-07-17 21:19:10 -0300

    added: user details routing with id's

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './core/pages/home/home.component';
 import { LoginComponent } from './core/pages/login/login.component';
 import { authGuard } from './core/guards/auth-guard.guard';
+import { UserDetailsComponent } from './core/components/user-details/user-details.component';
 
 const routes: Routes = [
   {path: "", component: HomeComponent},
   {path: "home", component: HomeComponent, canActivate: [authGuard]},
+  {path: "user/details/:userId", component: UserDetailsComponent, canActivate: [authGuard]},
   {path: "login", component: LoginComponent}
 ];
 
