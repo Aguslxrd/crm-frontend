e5ce12492fa815b73faa7e2105eb4783bcff4c0f
damian <suffodamian02@gmail.com>
2024-07-17 21:43:54 -0300

    added: user details pagination + cases templates

--- src/app/core/components/user-details/user-details.component.ts
+++ src/app/core/components/user-details/user-details.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UserService } from '../../services/user.service';
+import { UserInterface } from '../../interfaces/IUser';
 
 @Component({
   selector: 'app-user-details',
   templateUrl: './user-details.component.html',
-  styleUrl: './user-details.component.css'
+  styleUrls: ['./user-details.component.css']
 })
-export class UserDetailsComponent {
+export class UserDetailsComponent implements OnInit {
+  user: any | null = null; // cambiar a user interface
+  userId: number | null = null;
 
+  constructor(
+    private route: ActivatedRoute,
+    private userService: UserService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    const userIdParam = this.route.snapshot.paramMap.get('userId');
+    if (userIdParam) {
+      this.userId = parseInt(userIdParam, 10);
+      if (!isNaN(this.userId)) {
+        this.loadUserDetails(this.userId);
+      } else {
+        console.error('Invalid userId in URL');
+      }
+    }
+  }
+
+  loadUserDetails(userId: number): void {
+    this.userService.searchUserByUserId(userId).subscribe(
+      (user) => {
+        this.user = user;
+        console.log('User data:', this.user);
+      },
+      (error) => {
+        console.error('Error fetching user details:', error);
+      }
+    );
+  }
+
+  backButton() : void{
+    this.router.navigate(['/home'])
+  }
 }
