18b3d587ff80e1483989efcf8aa8f02c3e4c58c9
damian <suffodamian02@gmail.com>
2024-07-21 16:21:31 -0300

    added: enterprises interface, api url enterprises

---
+++ src/app/core/interfaces/IEnterprises.ts
@@ -0,0 +1,9 @@
+export interface EnterprisesInterface{
+    enterpriseid: number,
+    name_enterprise: string,
+    rut: string,
+    address: string,
+    phone: string,
+    email: string,
+    website: string
+}
--- src/app/core/services/enterprises.service.ts
+++ src/app/core/services/enterprises.service.ts
@@ -1,9 +1,13 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { StorageService } from './storage-service.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EnterprisesService {
+  private readonly apiUrl = 'http://localhost:8080/api/v1/enterprises';
 
-  constructor() { }
+  constructor(private http: HttpClient, private storageService: StorageService) {}
+
 }
