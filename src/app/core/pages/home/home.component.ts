f8c5b931b47abe3036f088c718a9b4e345cc8a5b
damian <suffodamian02@gmail.com>
2024-08-02 18:34:27 -0300

    added: get all cases count by status

--- src/app/core/pages/home/home.component.ts
+++ src/app/core/pages/home/home.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CasesService } from '../../services/cases.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  totalCases: number = 0;
+  openedCases: number = 0;
+  inProgressCases: number = 0;
 
+  constructor(private casesService: CasesService) {}
+
+  ngOnInit() {
+    this.loadCaseCounts();
+  }
+
+  loadCaseCounts() {
+    this.casesService.getAllCasesCount().subscribe({
+      next: (count) => this.totalCases = count,
+      error: (error) => console.error('Error fetching total cases:', error)
+    });
+
+    this.casesService.getAllOpenedCasesCount().subscribe({
+      next: (count) => this.openedCases = count,
+      error: (error) => console.error('Error fetching opened cases:', error)
+    });
+
+    this.casesService.getAllInProgressCasesCount().subscribe({
+      next: (count) => this.inProgressCases = count,
+      error: (error) => console.error('Error fetching in-progress cases:', error)
+    });
+  }
 }
