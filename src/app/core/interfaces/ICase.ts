31d377e5e0ec8ba94e2297d13fffdc8e99f5c70f
damian <suffodamian02@gmail.com>
2024-08-09 20:35:37 -0300

    added: authId on caseModule

--- src/app/core/interfaces/ICase.ts
+++ src/app/core/interfaces/ICase.ts
@@ -1,8 +1,9 @@
 export interface CaseInterface {
     caseId: number,
+    userId: number,
+    authId: number,
     title: string,
     description_case: string,
     date_created: Date,
-    case_status: string,
-    userId: number
+    case_status: string
 }
