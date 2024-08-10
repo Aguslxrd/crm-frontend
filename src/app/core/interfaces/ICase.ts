8cfb681fdb8412c7342cac7dfb87c134cd692c46
damian <suffodamian02@gmail.com>
2024-08-09 21:41:58 -0300

    added: now backend returns casedto, added adminName propertie

--- src/app/core/interfaces/ICase.ts
+++ src/app/core/interfaces/ICase.ts
@@ -2,6 +2,7 @@ export interface CaseInterface {
     caseId: number,
     userId: number,
     authId: number,
+    adminName: string,
     title: string,
     description_case: string,
     date_created: Date,
