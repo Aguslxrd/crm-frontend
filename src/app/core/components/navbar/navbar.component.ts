d41442b1264e44f26f27f27e75754d54533eb1f6
damian <suffodamian02@gmail.com>
2024-08-12 17:41:19 -0300

    added: under development alerts on new email,tasks buttons

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { StorageService } from '../../services/storage-service.service';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-navbar',
@@ -8,10 +9,14 @@ import { Router } from '@angular/router';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-  constructor(private storageService: StorageService, private router: Router){}
+  constructor(private storageService: StorageService, private router: Router, private toastr: ToastrService){}
 
   public logout(): void{
     this.storageService.clearAuth();
     this.router.navigate(['/login']);
+  }
+
+  developmentFeatureAlert(): void{
+    this.toastr.info('Bajo desarrollo!', 'ArcticCRM')
   }
 }
